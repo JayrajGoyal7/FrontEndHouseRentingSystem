a67ca162efaa3c0641c366de303152707e915e11
Jayraj Goyal <jayraj.goyal@einfochips.com>
2022-07-21 17:27:51 +0530

    UI feel for login and sigup process. Any other URL would lead to an error page.

---
+++ FrontEndHouseRentingSystem/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    NavBarComponent,
+    DashboardComponent,
+    LoginComponent,
+    SignupComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
